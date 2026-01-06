6298f4105431104d25280c4239855b4bf0ab0967
Alok Mahapatra <alokmahapatra2604@gmail.com>
2026-01-07 00:27:47 +0530

    hero video change

--- src/components/public/Hero.tsx
+++ src/components/public/Hero.tsx
@@ -93,7 +93,7 @@ export function Hero() {
                                     mixBlendMode: 'multiply'
                                 }}
                             >
-                                <source src="/animated.webm" type="video/webm" />
+                                <source src="/animated.mp4" type="video/mp4" />
                                 Your browser does not support the video tag.
                             </video>
                         </div>
